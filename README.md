dc41b85c4d468662279d78cf681e30a10902feec
Antti Virolainen <antti@sharetribe.com>
2018-04-09 13:33:46 +0300

    Add actual files

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This is a very simple ruby script that monitors a certain URL of a crowdfunding campaign page that has the raised sum visbile. The monitor plays a sound whenever it spots that the money amount has increased, and multiple sounds if the amount has increased a lot.
 
-The script works with Invesdor crowdfunding using EUR just by changing the URL, but for other platforms the code needs modifying. 
+The script works on Mac when monitoring Invesdor crowdfunding using EUR just by changing the URL, but for other platforms the code needs modifying. 
+
+The coin sound used is a freely downloadable ringtone from: http://www.orangefreesounds.com/mario-coin-sound/ and it can be easily replaced with another sound file if wanted.
---
+++ cf-sound-monitor.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/ruby
+require 'net/http'
+
+# This script reads an Invesdor campaign banner and plays 
+# sounds if the sum is bigger than last time this was run.
+
+
+# Config
+address = "https://www.invesdor.com/en/pitches/890/banner"
+sum_per_coin = 500
+filepath = "latest_sum.txt"
+soundpath = "coin.wav"
+repeat_after = 600 #number of seconds to wait between polls. Set 0 to turn of repeating.
+                    # NOTE: if this is above zero, the program never stops automatically
+
+
+while repeat_after > 0 do
+
+    print "\nChecking campaign status at #{Time.now}: "
+
+    # Read latest sum from file
+    latest_sum = 0
+    if File.file?(filepath)
+        File.open(filepath, 'r') do |f|  
+            latest_sum = f.gets.to_i  
+        end  
+    end
+
+    print "#{latest_sum} -> "
+
+
+    # Check latest sum online
+    uri = URI.parse(address)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    sum = response.body.match(/<span>([\d\.]+)..EUR<\/span>/)[1].delete(".").to_i
+
+    print  "#{sum}"
+
+
+    # Play the sounds
+    if sum > latest_sum
+        extra_coins = 0
+        diff = sum-latest_sum
+        if diff > sum_per_coin
+            extra_coins = (diff-501)/sum_per_coin + 1
+        end
+        print " x #{extra_coins + 1}"
+        
+        while extra_coins > 0 do
+            Thread.new do
+                `afplay #{soundpath}`
+            end
+            extra_coins -= 1
+            sleep 0.20
+        end
+        `afplay #{soundpath}`
+    end
+
+
+
+    # Write this sum to file
+    File.open(filepath, 'w') do |f|  
+        # use "\n" for two lines of text  
+        f.puts sum  
+    end 
+
+sleep repeat_after
+end
